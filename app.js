6a7f2e5dcc6609d0f58b51ab79d69f693ffb4c41
Sanjiv Shrestha <sanjeev.shrestha999@gmail.com>
2020-03-17 11:19:46 +0545

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("The server started @ 3000 port");
+});
