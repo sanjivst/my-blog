cfc4e7baec704150bf6ca248feba96a4ff2b16cf
Sanjiv Shrestha <sanjeev.shrestha999@gmail.com>
2020-03-22 17:46:33 +0545

    URI for each post and Truncate paragraph on homepage

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const contactContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const homeStartingContent = "Lorem ipsum dolor sit amet";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur laborum.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur laborum.";
 
 const app = express();
 
@@ -13,8 +14,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res){
-  res.render("home", {home: homeStartingContent});
+  res.render("home", {
+      home: homeStartingContent,
+      posts: posts
+    });
 });
 
 app.get("/about", function(req, res){
@@ -35,6 +41,19 @@ app.post("/compose", function(req, res){
       title: req.body.postTitle,
       content: req.body.postBody
     };
+
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function(post){
+      const storedTitle = _.lowerCase(post.title);
+      if (storedTitle == requestedTitle){
+        res.render("post", {title: post.title, body: post.content});
+      }
+  });
 });
 
 app.listen(3000, function(){
